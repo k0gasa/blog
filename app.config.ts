960b4564202feeaef9d2b34b054f099dfc468a31
nikolas <malacalypse@proton.me>
2024-10-03 15:42:02 +0100

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 // https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'kogasa',
+    description: 'sci blog',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -19,21 +19,24 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'k0gasa',
+      instagram: '',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      github: 'k0gasa'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
